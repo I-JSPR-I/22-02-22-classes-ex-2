6314dd163e6f1681e1b8eaa4945b45ab47a120f6
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-02-24 19:56:42 +0100

    Finished

--- 22-02-22 classes ex 2/Program.cs
+++ 22-02-22 classes ex 2/Program.cs
@@ -6,7 +6,14 @@ namespace _22_02_22_classes_ex_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+                int OpvragenTeller, OpvragenNoemer;
+            Console.WriteLine("Wat is de teller?");
+            OpvragenTeller= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wat is de noemer?");
+            OpvragenNoemer = Convert.ToInt32(Console.ReadLine());
+            Storage OpvragenBreuk = new Storage(OpvragenTeller,OpvragenNoemer);
+            Console.WriteLine($"De uitkomst van {OpvragenTeller}/{OpvragenNoemer} is {OpvragenBreuk.breuk}");
         }
     }
 }
---
+++ 22-02-22 classes ex 2/Storage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_02_22_classes_ex_2
+{
+    class Storage
+    {
+        public int teller;
+        public int noemer;
+        public double breuk;
+        public Storage()
+        {
+           teller = 20;
+            noemer = 10;
+           
+        }
+        public Storage (int OpvragenTeller, int OpvragenNoemer)
+        {
+            
+            teller = OpvragenTeller;
+            noemer = OpvragenNoemer;
+            breuk = teller / noemer;
+        }
+    }
+}
